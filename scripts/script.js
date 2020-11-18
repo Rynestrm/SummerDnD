c399a6f3d7711adbcf3958a83fff18e4d578d62c
Ivan <gibson.ivan@gmail.com>
2020-11-18 15:36:14 -0500

    small changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,68 @@
+// variables
 const monsterList = [];
-const searchForMonsters = document.querySelector('input');
-const monsterName = document.querySelector('#monster');
+const searchForMonsters = document.querySelector("input");
+const monsterName = document.querySelector("#monster");
+let monsters = document.querySelector(".monsters");
 
-async function getMonsters(){
-    const responce = await fetch('https://gist.githubusercontent.com/tkfu/9819e4ac6d529e225e9fc58b358c3479/raw/d4df8804c25a662efc42936db60cfbc0a5b19db8/srd_5e_monsters.json');
-    const data = await responce.json();
-    monsterList.push(...data);
-    // console.log(data);
+// getting json data
+async function getMonsters() {
+  const responce = await fetch(
+    "https://gist.githubusercontent.com/tkfu/9819e4ac6d529e225e9fc58b358c3479/raw/d4df8804c25a662efc42936db60cfbc0a5b19db8/srd_5e_monsters.json"
+  );
+  const data = await responce.json();
+  monsterList.push(...data);
 }
 getMonsters();
 
-searchForMonsters.onkeyup = function(keyEvent) {
-  const typedText = keyEvent.target.value
-  console.log(typedText)
-  const resultingArray = monsterList.filter
-  (function(monster) {
-    return monster.name.toLowerCase(typedText).includes(typedText.toLowerCase())
-  })
-  console.log(resultingArray)
-  monsterName.innerHTML = JSON.stringify(resultingArray, null, 2)
-}
+// search the object
+searchForMonsters.onkeyup = function (keyEvent) {
+  const typedText = keyEvent.target.value;
+  const resultingArray = monsterList.filter(function (monster) {
+    return monster.name
+      .toLowerCase(typedText)
+      .includes(typedText.toLowerCase());
+  });
+  // monsterName.innerHTML = JSON.stringify(resultingArray, null, 2);
+  // render(resultingArray);
+};
 
-// const resultingArray = monsterList.filter(monster => monster.name.toLowerCase(typedText).includes(typedText))
+// const outPut = monsterSearch.map(function (monsters) {
+//   return`test`
+// })
+
+// function render(resultingArray) {
+//   resultingArray.map(function(monster) {
+//     // console.log(resultingArray);
+//     return `
+//     <pre class="monsters">
+//     <h2>${monster.name}</h2>
+//     <h3>Meta</h3>
+//     <h3>Armor Class</h3>
+//     <div>
+//       <h3>Traits</h3>
+//       <p>Trait text</p>
+//     </div>
+//     <div>
+//       <h3>Actions</h3>
+//       <p>Action Text</p>
+//     </div>
+//     <div>
+//       <h3>Legendary Actions</h3>
+//       <p>Legendary Text</p>
+//     </div>
+//     <div>
+//       <img src="" alt="monster image">
+//     </div>
+//   </pre>`
+//   });
+// }
+
+// monsters.innerHTML = outPut;
+
+/* Name
+Meta
+Armor Class
+Hit Points
+Speed
+Strength
+Strength Mod */
